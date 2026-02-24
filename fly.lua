77fd98cc7b6dde47607543f3d2cce1fb692482d3
ilaygermon1-ship-it <ilaygermon1@gmail.com>
2026-02-24 19:04:00 +0200

    Update fly.lua

--- fly.lua
+++ fly.lua
@@ -1,5 +1,6 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
+-- יצירת החלון
 local Window = Rayfield:CreateWindow({
    Name = "ILhub | Survive Lava",
    LoadingTitle = "ILhub Loading...",
@@ -11,24 +12,40 @@ local Window = Rayfield:CreateWindow({
    }
 })
 
--- טאב לשליטה בדמות
-local PlayerTab = Window:CreateTab("Player", 4483362458) -- אייקון של משתמש
+-- משתנה לשמירת מצב התפריט (פתוח/סגור)
+local ui_open = true
+
+-- פונקציה להצגה והסתרה
+local function toggleUI()
+    ui_open = not ui_open
+    if ui_open then
+        Window:Open() -- פותח את התפריט
+    else
+        Window:Close() -- סוגר את התפריט
+    end
+end
+
+-- זיהוי לחיצה על המקש T
+game:GetService("UserInputService").InputBegan:Connect(function(input, gpe)
+    if not gpe and input.KeyCode == Enum.KeyCode.T then
+        toggleUI()
+    end
+end)
+
+-- יצירת טאב Player
+local PlayerTab = Window:CreateTab("Player", 4483362458)
 
 local Section = PlayerTab:CreateSection("Movement")
 
--- כפתור Fly
+-- כפתור תעופה (Fly)
 PlayerTab:CreateToggle({
    Name = "Fly (Press F)",
    CurrentValue = false,
    Flag = "FlyToggle",
    Callback = function(Value)
       _G.Flying = Value
-      if Value then
-          -- כאן נכנסת פונקציית התעופה שלך
-          print("Fly Enabled")
-      else
-          print("Fly Disabled")
-      end
+      -- כאן תוכל להוסיף את לוגיקת התעופה שרצית
+      if Value then print("Fly On") else print("Fly Off") end
    end,
 })
 
@@ -45,26 +62,10 @@ PlayerTab:CreateSlider({
    end,
 })
 
--- סליידר לקפיצה
-PlayerTab:CreateSlider({
-   Name = "JumpPower",
-   Range = {50, 500},
-   Increment = 1,
-   Suffix = "Power",
-   CurrentValue = 50,
-   Flag = "JumpSlider",
-   Callback = function(Value)
-      game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-   end,
+-- הודעה למשתמש
+Rayfield:Notify({
+   Title = "ILhub Loaded!",
+   Content = "Press 'T' to Hide/Show the Menu",
+   Duration = 5,
+   Image = 4483362458,
 })
-
--- טאב הגדרות
-local SettingsTab = Window:CreateTab("Settings", 4483362458)
-SettingsTab:CreateButton({
-   Name = "Destroy UI",
-   Callback = function()
-      Rayfield:Destroy()
-   end,
-})
-
-Rayfield:LoadConfiguration()
